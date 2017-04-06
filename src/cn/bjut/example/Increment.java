ae6315df8734221df905d0f5294ba57c01af426a
HL123 <1939018754@qq.com>
2017-04-06 23:27:42 +0800

    add 2

--- src/cn/bjut/example/Increment.java
+++ src/cn/bjut/example/Increment.java
@@ -4,4 +4,17 @@ package cn.bjut.example;
  * Created by huanglei on 17/4/6.
  */
 public class Increment {
+    public static void main(String[] args) {
+        int j = 0;
+        for (int i = 0; i < 100; i++) {
+            j = j++;
+        }
+        System.out.println(j);
+
+
+        int m = 0;
+        m= m++;
+        System.out.println(m);
+
+    }
 }
